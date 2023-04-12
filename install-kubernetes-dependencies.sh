0f97b97fa282078934555173be4b2e0ad7988fbc
Oliver Michels <oliver.michels@gmail.com>
2023-04-12 09:46:47 +0200

    ensure exit code = 0

--- install-kubernetes-dependencies.sh
+++ install-kubernetes-dependencies.sh
@@ -69,12 +69,10 @@ install_cri_dockerd ()
 {
 cd /tmp
 export VER="0.3.1"
-wget https://github.com/Mirantis/cri-dockerd/releases/download/v${VER}/cri-dockerd-${VER}.amd64.tgz
+wget -q https://github.com/Mirantis/cri-dockerd/releases/download/v${VER}/cri-dockerd-${VER}.amd64.tgz
 tar xvf cri-dockerd-${VER}.amd64.tgz
 
 sudo mv cri-dockerd/cri-dockerd /usr/bin/
-
-cri-dockerd --version
 
 wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/master/packaging/systemd/cri-docker.service
 wget https://raw.githubusercontent.com/Mirantis/cri-dockerd/master/packaging/systemd/cri-docker.socket
@@ -83,6 +81,8 @@ sudo mv cri-docker.socket cri-docker.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable cri-docker.service
 sudo systemctl enable --now cri-docker.socket
+
+cri-dockerd --version
 }
 
 
